74ae83709ea58bec2dea4eb6a5b60c6345c6d0ee
Gerva02 <gerva02@gmail.com>
2023-03-23 17:18:54 +0100

    description of the problem

---
+++ HM_2.R
@@ -0,0 +1,7 @@
+#HOMEWORK
+#-----------------------------------------------------------
+# Caricare e descrivere il dataset gala (library faraway).
+# Fare una regressione lineare semplice di
+# Species (risposta) ed Elevation (esplicativa), commentando il
+# summary, costruendo IC e I di previsione
+#-----------------------------------------------------------

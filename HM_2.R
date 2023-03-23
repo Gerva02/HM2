4c5ccb82316355c24d90c18f7e01d4caf71b49cb
Gerva02 <gerva02@gmail.com>
2023-03-23 17:44:26 +0100

    Linear model

--- HM_2.R
+++ HM_2.R
@@ -5,3 +5,19 @@
 # Species (risposta) ed Elevation (esplicativa), commentando il
 # summary, costruendo IC e I di previsione
 #-----------------------------------------------------------
+
+#loading the right dataset
+library("faraway")
+data(gala)
+
+
+#Elevation is our dependent variable , species is our independent variable 
+plot(Elevation ~ Species, data= gala)
+(mod <- lm(Elevation ~ Species, data= gala))
+abline(mod, col ="red", lty ="dotted") #plotting our linear regression model
+(sum.mod <- summary(mod))
+
+
+
+
+
